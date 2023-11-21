018d205a9febc974a9065c81ea1a5054f358c780
controlnumerico <info@controlnumericomexico.com>
2023-11-21 15:26:20 -0600

    new fl mgr

--- cnm/mainwindow.py
+++ cnm/mainwindow.py
@@ -1,5 +1,10 @@
+import os
+
 from qtpyvcp.widgets.form_widgets.main_window import VCPMainWindow
 from PyQt5.QtWidgets import QStatusBar, QLabel
+from qtpyvcp.actions.program_actions import load, run
+from qtpyvcp.utilities.info import Info
+
 
 # Setup logging
 from qtpyvcp.utilities import logger
@@ -14,6 +19,9 @@ class MyMainWindow(VCPMainWindow):
         self.edit = self.findChild(QStatusBar, "statusbar")
         self.edit.messageChanged.connect(self.changeText)
 
+        self.btn_loadrun.clicked.connect(self.load_then_run)
+
+
     def changeText(self):
         self.label.setText(self.edit.currentMessage())
 
@@ -21,4 +29,13 @@ class MyMainWindow(VCPMainWindow):
 
     def on_exitAppBtn_clicked(self):
         self.app.quit()
+
+   # add any custom methods here
+    def load_then_run(self):
+        # now we load the prog
+        info = Info()
+        nc_files_dir = info.getProgramPrefix()
+        path = os.path.expanduser(nc_files_dir)
+        load(path + "/axis.ngc")
+#        run()
 
